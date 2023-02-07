ff49114bc95b3a033747e225a9268963491beb24
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-02-07 19:17:54 +0100

    Number 9

--- src/RomanNumerals/ArabicToRomanConverter.cs
+++ src/RomanNumerals/ArabicToRomanConverter.cs
@@ -10,27 +10,26 @@ public class ArabicToRomanConverter
 
     public string Convert(int number)
     {
+        if (number == GetNumber(number))
+            return romanNumbers[number];
+
         string result = string.Empty;
 
-        if (number == GetNumber(number))
-        {
-            return romanNumbers[number];
-        }
-
         if (number == GetSmallerNumber(number) * 4)
-        {
             result = Convert(GetSmallerNumber(number)) + Convert(GetGreaterNumber(number));
-        }
+        else if (number == GetSecondSmallerNumber(number) * 9)
+            result = Convert(GetSecondSmallerNumber(number)) + Convert(GetGreaterNumber(number));
         else
-        {
             result = Convert(GetSmallerNumber(number)) + Convert(number - GetSmallerNumber(number));
-        }
 
         return result;
     }
 
     private int GetNumber(int number)
         => romanNumbers.Keys.Where(n => n == number).FirstOrDefault();
+
+    private int GetSecondSmallerNumber(int number)
+        => GetSmallerNumber(GetSmallerNumber(number));
 
     private int GetSmallerNumber(int number)
         => romanNumbers.Keys.Where(n => n < number).LastOrDefault();
--- test/RomanNumeralsTests/Tests.cs
+++ test/RomanNumeralsTests/Tests.cs
@@ -8,13 +8,15 @@ public class Tests
     public void ConvertFromArabicToRoman()
     {
         AssertConvertion(1, "I");
-        AssertConvertion(5, "V");
-        AssertConvertion(10, "X");
         AssertConvertion(2, "II");
         AssertConvertion(3, "III");
         AssertConvertion(4, "IV");
+        AssertConvertion(5, "V");
         AssertConvertion(6, "VI");
         AssertConvertion(7, "VII");
+        AssertConvertion(8, "VIII");
+        AssertConvertion(10, "X");
+        AssertConvertion(9, "IX");
     }
 
     private static void AssertConvertion(int Number, string Expected)
