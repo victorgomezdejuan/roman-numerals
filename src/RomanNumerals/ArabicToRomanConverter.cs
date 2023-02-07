d0e021881ed4f139803e66c6b836f28119be1e4e
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-02-07 17:59:49 +0100

    Number 4

--- src/RomanNumerals/ArabicToRomanConverter.cs
+++ src/RomanNumerals/ArabicToRomanConverter.cs
@@ -1,7 +1,8 @@
 ﻿namespace RomanNumerals;
 public class ArabicToRomanConverter
 {
-    private Dictionary<int, string> romanNumbers = new() {
+    private Dictionary<int, string> romanNumbers = new()
+    {
         [1] = "I",
         [5] = "V",
         [10] = "X"
@@ -11,8 +12,12 @@ public class ArabicToRomanConverter
     {
         string result = string.Empty;
         int nextSmallerNumber = romanNumbers.Keys.Where(n => n <= number).Max();
+        int nextGreaterNumber = romanNumbers.Keys.Where(n => n == number + nextSmallerNumber).FirstOrDefault();
         
-        for(int i = number; i > 0; i -= nextSmallerNumber)
+        if (nextSmallerNumber != number && (nextGreaterNumber - nextSmallerNumber == number))
+            return romanNumbers[nextSmallerNumber] + romanNumbers[nextGreaterNumber];
+
+        for (int i = number; i > 0; i -= nextSmallerNumber)
             result += romanNumbers[nextSmallerNumber];
 
         return result;
--- test/RomanNumeralsTests/Tests.cs
+++ test/RomanNumeralsTests/Tests.cs
@@ -5,12 +5,14 @@ namespace RomanNumeralsTests;
 public class Tests
 {
     [Fact]
-    public void Convert1FromArabicToRoman()
+    public void ConvertFromArabicToRoman()
     {
         AssertConvertion(1, "I");
         AssertConvertion(5, "V");
         AssertConvertion(10, "X");
         AssertConvertion(2, "II");
+        AssertConvertion(3, "III");
+        AssertConvertion(4, "IV");
     }
 
     private static void AssertConvertion(int Number, string Expected)
